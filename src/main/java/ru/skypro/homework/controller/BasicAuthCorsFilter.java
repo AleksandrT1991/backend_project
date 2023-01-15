15df1d15d15d32f5daf641892c8ed95de41a50e3
RadiumR30 <radiumr30@gmail.ru>
2023-01-15 11:44:40 +0400

    123

--- src/main/java/ru/skypro/homework/controller/BasicAuthCorsFilter.java
+++ src/main/java/ru/skypro/homework/controller/BasicAuthCorsFilter.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 @Component
 public class BasicAuthCorsFilter extends OncePerRequestFilter {
-//123
+//
     @Override
     protected void doFilterInternal(HttpServletRequest httpServletRequest,
                                     HttpServletResponse httpServletResponse,
