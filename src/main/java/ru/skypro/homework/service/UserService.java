6667be6d7ce3bbae4569653e0c5f4496112887a1
Yan Tuzlukov <tyzluckoff@yandex.ru>
2023-01-15 21:18:08 +0700

    add services, controllers, dto

---
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -0,0 +1,7 @@
+package ru.skypro.homework.service;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserService {
+}
---
+++ src/main/java/ru/skypro/homework/service/impl/AdsServiceImpl.java
@@ -0,0 +1,50 @@
+package ru.skypro.homework.service.impl;
+
+import org.springframework.stereotype.Service;
+import ru.skypro.homework.dto.AdDto;
+
+@Service
+public class AdsServiceImpl {
+
+    public void getAd(AdDto ad) {
+    }
+
+    public void addAd(AdDto ad) {
+
+    }
+
+    public void getComments(String adPk) {
+
+    }
+
+    public void addComments(String adPk) {
+
+    }
+
+    public void getFullAd(Integer id) {
+
+    }
+
+    public void removeAds(Integer id) {
+
+    }
+
+    public void updateAds(Integer id) {
+
+    }
+
+    public void getComments(Integer id, String adPk) {
+
+    }
+
+    public void deleteComments(Integer id, String adPk) {
+    }
+
+    public void updateComments(Integer id, String adPk) {
+
+    }
+
+    public void getAdsMe(AdDto ad) {
+
+    }
+}
