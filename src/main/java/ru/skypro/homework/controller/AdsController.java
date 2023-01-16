5918fa4c7e66a2fdccb39c4b1728d8aa1911b323
Yan Tuzlukov <tyzluckoff@yandex.ru>
2023-01-17 02:18:58 +0700

    fix after review

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -23,47 +23,47 @@ public class AdsController {
         adsService.addAd(ad);
     }
 
-    @GetMapping("/ads/{adPk}/comments")
+    @GetMapping("/{adPk}/comments")
     public void getComments(@PathVariable String adPk) {
         adsService.getComments(adPk);
     }
 
-    @GetMapping("/ads/{adPk}/comments")
+    @PostMapping("/{adPk}/comments")
     public void addComments(@PathVariable String adPk) {
         adsService.addComments(adPk);
     }
 
-    @GetMapping("/ads/{id}")
+    @GetMapping("/{id}")
     public void getFullAd(@PathVariable Integer id) {
         adsService.getFullAd(id);
     }
 
-    @DeleteMapping("/ads/{id}")
+    @DeleteMapping("/{id}")
     public void removeAds(@PathVariable Integer id) {
         adsService.removeAds(id);
     }
 
-    @PatchMapping("/ads/{id}")
+    @PatchMapping("/{id}")
     public void updateAds(@PathVariable Integer id) {
         adsService.updateAds(id);
     }
 
-    @GetMapping("/ads/{adPk}/comments/{id}")
+    @GetMapping("/{adPk}/comments/{id}")
     public void getComments(@PathVariable Integer id, @PathVariable String adPk) {
         adsService.getComments(id, adPk);
     }
 
-    @DeleteMapping("/ads/{adPk}/comments/{id}")
+    @DeleteMapping("/{adPk}/comments/{id}")
     public void deleteComments(@PathVariable Integer id, @PathVariable String adPk) {
         adsService.deleteComments(id, adPk);
     }
 
-    @PatchMapping("/ads/{adPk}/comments/{id}")
+    @PatchMapping("/{adPk}/comments/{id}")
     public void updateComments(@PathVariable Integer id, @PathVariable String adPk) {
         adsService.updateComments(id, adPk);
     }
 
-    @GetMapping("/ads/me")
+    @GetMapping("/me")
     public void getAdsMe(@RequestBody AdDto ad) {
         adsService.getAdsMe(ad);
     }
