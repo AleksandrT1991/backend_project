9db8acfff3ebf443e7cb1b2eb0b8d17deea1c35a
AleksandrTsygulev <aleksandr.tsygulev@gmail.com>
2023-02-04 18:53:11 +0300

    05.02 updateUser, imageUpdateUser, getUser

--- src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
+++ src/main/java/ru/skypro/homework/service/impl/UserServiceImpl.java
@@ -1,49 +1,35 @@
 package ru.skypro.homework.service.impl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.entity.User;
+import ru.skypro.homework.entity.UserImage;
+import ru.skypro.homework.repository.user.UserImageRepository;
 import ru.skypro.homework.repository.user.UserRepository;
 import ru.skypro.homework.service.UserService;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final UserImageRepository userImageRepository;
 
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, UserImageRepository userImageRepository) {
         this.userRepository = userRepository;
+        this.userImageRepository = userImageRepository;
     }
-//
-//    public void updateUserImage(Long userId, MultipartFile file) throws IOException {
-//        Optional<User> user = findUser(userId);
-//        Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(file.getOriginalFilename()));
-//        Files.createDirectories(filePath.getParent());
-//        Files.deleteIfExists(filePath);
-//
-//        try (InputStream is = file.getInputStream();
-//             OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
-//             BufferedInputStream bis = new BufferedInputStream(is, 1024);
-//             BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
-//        ) {
-//            bis.transferTo(bos);
-//        }
-//        UserImage userImage = userImageRepository.findByUserId(userId).orElseGet(UserImage::new);
-//        userImage.setUser(user);
-//        userImage.setFilePath(filePath.toString());
-//        userImage.setFileSize(file.getSize());
-//        userImage.setMediaType(file.getContentType());
-//        userImage.setData(file.getBytes());
-//
-//        userRepository.save(userImage);
-//    }
-
-//    @Override
-//    public User setPassword (String currentPassword, String newPassword) {
-//        return userRepository.save(currentPassword, newPassword);
-//    }
+    @Value("")
+    private String imageDir;
 
     @Override
     public User addUser(User user) {
@@ -75,4 +61,43 @@ public class UserServiceImpl implements UserService {
     public void deleteById(Long id) {
         userRepository.deleteById(id);
     }
+
+    @Override
+    public Optional<User> updateUser(User user) {
+        Optional<User> optional = userRepository.findById(user.getId());
+        if (!optional.isPresent()) {
+            return Optional.empty();
+        } else {
+            user.setId(optional.get().getId());
+            return Optional.of(userRepository.save(user));
+        }
+    }
+
+    @Override
+    public void updateUserImage(Long id, MultipartFile file) throws IOException {
+        Optional<User> user = userRepository.findById(id);
+        Path filePath = Path.of(imageDir, id + "." + getExtensions(file.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+        try (InputStream is = file.getInputStream();
+             OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+             BufferedInputStream bis = new BufferedInputStream(is, 1024);
+             BufferedOutputStream bos = new BufferedOutputStream(os, 1024)
+        ) {
+            bis.transferTo(bos);
+        }
+        UserImage userImage = userImageRepository.findByUserId(id);
+        userImage.setUserId(id);
+        userImage.setFilePath(filePath.toString());
+        userImage.setFileSize(userImage.getFileSize());
+        userImage.setMediaType(userImage.getMediaType());
+        userImageRepository.save(userImage);
+    }
+    private String getExtensions(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+//    @Override
+//    public void setPassword() {
+//        userRepository.setPassword();
+//    }
 }
